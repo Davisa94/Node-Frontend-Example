72018364c048131cb0e856425d461ca3c3f0f8e3
Davisa94 <Austin.i.davis94@gmail.com>
2020-11-09 11:50:23 -0700

    Frontend skeleton complete

---
+++ src/components/Form.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container>
+        <dd-form
+           :data="exampleDataSet"
+           :descriptions="exampleDescriptions"
+           @submit="submit">
+        </dd-form>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "Form",
+    data:() => ({
+        exampleDataSet: {},
+        exampleDescriptions: {
+      "name": { view: 'text', label: 'Full name'}
+  }
+    })
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import ddForm from 'vue-dd-form';
 
 Vue.config.productionTip = false
-
+Vue.component('dd-form', ddForm); 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
